cf05a73685dd7493a7a04a67f171d0ca3f91a5e2
Dmitry.k <dmitry.k792@gmail.com>
2019-02-15 18:58:55 +0200

    Templates

---
+++ File Templates/Personal/Swift File.xctemplate/ClassCheckedUICollectionViewCellXIB/___FILEBASENAME___.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_subclassSwift:identifier___ {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
